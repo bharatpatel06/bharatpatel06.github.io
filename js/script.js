c6a755016f8a933509830d8254f6659a1a3400c2
Bharat Patel <bharat06patel@gmail.com>
2017-08-26 16:45:01 +0530

    Ajax Call bug fixes

--- js/script.js
+++ js/script.js
@@ -15,26 +15,30 @@ $(window).on("load", function(){
 
 	///////////////////////////////////////////////////////////Load My Works////////////////////////////////////////////////
 
-	var flag=0;
+	var flag=0, link = "", title = "", imgSrc = "";
 		if(flag==0)
 		{
 			flag=1;
-			var yql = encodeURIComponent("select * from xml where url='https://codepen.io/bharatpatel/public/feed/'");
-
-			yql = "https://query.yahooapis.com/v1/public/yql?q="+yql+"&format=json&callback=?"
-			$.getJSON(yql, function(data) {
-				var items = data.query.results.rss.channel.item;
-				for(var i=0; i<6; i++)
-				{
-					var parser = new DOMParser(), doc = parser.parseFromString(items[i].description, "text/xml");
-					var imgSrc = items[i].link+"/image/large.png";
-					$(".loadRSS").append("<div class='workTabs col-xs-4 col-md-4 text-center'><a target='_blank' href='"+items[i].link.replace('/pen/', '/full/')+"'><img src="+imgSrc+" alt='"+items[i].subject+"'><br>"+ items[i].subject +"</a></div>");
-					if((i+1)%3 == 0)
-						$(".loadRSS").append("<div class='clearfix'></div>");
-					// if((i+1)%2 == 0)
-					// 	$(".loadRSS").append("<div class='clearfix hidden-md hidden-lg'></div>");
+			var yql = "https://codepen.io/bharatpatel/public/feed";
+			$.ajax({
+                url:        yql,
+                dataType:   "html",
+                success:    function(data){
+                	data = $.parseHTML(data);
+                	var items = data[0].getElementsByTagName("item");
+					for(var i=0 ; i< 6; i++)
+					{
+						link = items[i].children[2].innerHTML;
+						title = items[i].getElementsByTagName("title")[0].innerHTML;
+						imgSrc = link+"/image/large.png";
+						$(".loadRSS").append("<div class='workTabs col-xs-4 col-md-4 text-center'><a target='_blank' href='"+link.replace('/pen/', '/full/')+"'><img src="+imgSrc+" alt='"+ title +"'><br>"+ title +"</a></div>");
+						if((i+1)%3 == 0)
+							$(".loadRSS").append("<div class='clearfix'></div>");
+						// if((i+1)%2 == 0)
+						// 	$(".loadRSS").append("<div class='clearfix hidden-md hidden-lg'></div>");
+					}
 				}
-			}, "jsonp");
+			});
 		}
 
 	///////////////////////////////////////////////////////////Load My Works////////////////////////////////////////////////
